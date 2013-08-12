4dc6d9b012371a39af89bfd09a37d2b1528b1d91
Chris <Topher253@gmail.com>
2013-08-12 12:07:38 -0700

    Begin Wordpress Final plugin

---
+++ WP Map Maker/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Map Maker
+Plugin URI: http://github.com/tollmanz/wordpress-development-course
+Version: 1.0
+Description: Fully customizable maps intergrated into your wordpress theme
+Author: Chris Allen
+Author URI: http://cpallen.com
+License: GNU General Public License v2 or later
+*/
