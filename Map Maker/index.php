a47e26dabe998533512f8a0a158377bb6a62d898
Chris <Topher253@gmail.com>
2013-08-19 12:28:48 -0700

    Enqueue styles
    Worked on testing different features of Map box API by putting the code
    in the body.

--- Map Maker/index.php
+++ Map Maker/index.php
@@ -8,3 +8,22 @@ Author: Chris Allen
 Author URI: http://cpallen.com
 License: GNU General Public License v2 or later
 */
+
+function map_maker_enqueue_script() {
+	    wp_enqueue_script(
+	        'Map Box API',
+	        '//api.tiles.mapbox.com/mapbox.js/v1.3.1/mapbox.js',
+	        array(),
+	        null,
+	        true
+	    );
+}
+
+function map_maker_enqueue_style() {
+		wp_enqueue_style(
+		    'Map Box Styles',
+		    plugins_url( '//api.tiles.mapbox.com/mapbox.js/v1.3.1/mapbox.css', __FILE__ ),
+		    array(),
+		    'all'
+		);
+}
