d8e2dadc2afc326450004bbf175081b9c5ef536d
Chris <Topher253@gmail.com>
2013-09-08 22:15:29 -0700

    Attempting to fix up user experience on settings

--- Map Maker/index.php
+++ Map Maker/index.php
@@ -16,6 +16,14 @@ License: GNU General Public License v2 or later
  *
  * @return void
  */
+
+add_action( 'admin_enqueue_scripts', 'mw_enqueue_color_picker' );
+function mw_enqueue_color_picker( $hook_suffix ) {
+    // first check that $hook_suffix is appropriate for your admin page
+    wp_enqueue_style( 'wp-color-picker' );
+    wp_enqueue_script( 'my-script-handle', plugins_url('my-script.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+}
+
 function cpa_map_maker_enqueue_script() {
     wp_enqueue_script(
         'Map Box API',
@@ -55,22 +63,42 @@ add_action( 'wp_enqueue_scripts', 'cpa_map_maker_enqueue_style' );
  * @return void
  */
 function cpa_map_add_custon_map( ) {
-
+  
   ?>
      <div id="map"></div>
-     <style> #map { height: 250px; } </style>
-     <script type="text/javascript">
-      var map = L.mapbox.map("map", "topher253.map-emb9xcho")
-     </script>
-   <?php  
-     $content .= $button_html;
-  
-  
-   echo $content;
+       <style> #map { height: 150px;} </style>
+       <script type="text/javascript">
+       <?php $mapZoom = get_option( 'marker_custom_zoom', 3 ); ?>
+       <?php $markerlonLocation = get_option( 'marker_custom_lon' ); ?>
+       <?php $markerlatLocation = get_option( 'marker_custom_lat' ); ?>
+       <?php $mapID = get_option( 'marker_custom_hex' ); ?>
+       <?php $markerTitle = get_option( 'marker_custom_title'); ?>
+       <?php $markerDescription = get_option( 'marker_custom_description'); ?>
+          var map = L.mapbox.map("map", "topher253.map-emb9xcho")
+
+            .setView([<?php echo $markerlatLocation ?>,<?php echo $markerlonLocation ?> ], <?php echo $mapZoom ?>);
+
+          var geoJson = [{
+              type: 'Feature',
+              geometry: {
+                  type: 'Point',
+                  coordinates: [<?php echo $markerlonLocation ?>, <?php echo $markerlatLocation ?>]
+              },
+              properties: {
+                  title: '<?php echo $markerTitle ?>',
+                  description: '<?php echo $markerDescription ?>',
+                  // http://mapbox.com/developers/simplestyle/
+                  'marker-color': '#<?php echo $mapID ?>'
+              }
+          }];
+          map.markerLayer.setGeoJSON(geoJson);
+       </script>   
+  <?php  
+   
 }
 
 add_action( 'wp_footer', 'cpa_map_add_custon_map', 20 );
-add_shortcode( 'mapbox', 'cpa_map_add_custon_map');
+add_shortcode( 'Map Maker', 'cpa_map_add_custon_map');
 
 /**
  * Add an options page for the plugin.
@@ -83,7 +111,7 @@ add_shortcode( 'mapbox', 'cpa_map_add_custon_map');
 function cpa_map_add_options_page() {
   add_options_page(
     __('Mapbox Options'),
-    __('mapbox Options'),
+    __('Map Maker Editor'),
     'manage_options',
     'map_options_page',
     'cpa_map_render_options_page'
@@ -114,12 +142,15 @@ function cpa_map_render_options_page() {
              <div id="map"></div>
              <style> #map { height: 150px; width: 50%;} </style>
              <script type="text/javascript">
+             <?php $mapZoom = get_option( 'marker_custom_zoom', 3 ); ?>
              <?php $markerlonLocation = get_option( 'marker_custom_lon' ); ?>
              <?php $markerlatLocation = get_option( 'marker_custom_lat' ); ?>
-             <?php $mapID = get_option( 'marker_custom_hex' ); ?>
+             <?php $mapID = get_option( 'marker_custom_hex'); ?>
+             <?php $markerTitle = get_option( 'marker_custom_title'); ?>
+             <?php $markerDescription = get_option( 'marker_custom_description'); ?>
                 var map = L.mapbox.map("map", "topher253.map-emb9xcho")
 
-                  .setView([45.52, -122.68], 5);
+                  .setView([<?php echo $markerlatLocation ?>,<?php echo $markerlonLocation ?> ], <?php echo $mapZoom ?>);
 
                 var geoJson = [{
                     type: 'Feature',
@@ -128,7 +159,8 @@ function cpa_map_render_options_page() {
                         coordinates: [<?php echo $markerlonLocation ?>, <?php echo $markerlatLocation ?>]
                     },
                     properties: {
-                        title: 'Portland Fucking Oregon',
+                        title: '<?php echo $markerTitle ?>',
+                        description: '<?php echo $markerDescription ?>',
                         // http://mapbox.com/developers/simplestyle/
                         'marker-color': '#<?php echo $mapID ?>'
                     }
@@ -139,6 +171,7 @@ function cpa_map_render_options_page() {
                 <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e( 'Update Changes' ); ?>">
              </p>
         </form>
+        <h3>Now go use the [Map Maker] shortcode where you would like this map to display</h3>
     </div>
 <?php
 }
@@ -160,13 +193,13 @@ function cpa_customize_map_settings() {
 
   register_setting(
     'marker_custom_settings',
-    'marker_custom_lon'
+    'marker_custom_zoom'
   );
 
   add_settings_field(
-    'marker_custom_location_field',
-    __( 'Input marker Location:'),
-    'cpa_map_render_custom_marker_input',
+    'marker_custom_zoom_field',
+    __( 'Select a default zoom:'),
+    'cpa_map_render_zoom_input',
     'map_options_page',
     'map_main_settings'
   );
@@ -186,16 +219,56 @@ function cpa_customize_map_settings() {
 
   register_setting(
     'marker_custom_settings',
+    'marker_custom_lon'
+  );
+
+  add_settings_field(
+    'marker_custom_location_field',
+    __( 'Marker Location:'),
+    'cpa_map_render_custom_marker_input',
+    'map_options_page',
+    'map_main_settings'
+  );
+
+  register_setting(
+    'marker_custom_settings',
     'marker_custom_hex'
   );
 
   add_settings_field(
     'marker_custom_hex_field',
-    __( 'Input Marker Hex #:'),
+    __( 'Marker Hex #:'),
     'cpa_map_render_custom_map_input',
     'map_options_page',
     'map_main_settings'
   );
+
+  register_setting(
+    'marker_custom_settings',
+    'marker_custom_title'
+  );
+
+  add_settings_field(
+    'marker_custom_title_field',
+    __( 'Marker Title:'),
+    'cpa_map_render_marker_title_input',
+    'map_options_page',
+    'map_main_settings'
+  );
+
+  register_setting(
+    'marker_custom_settings',
+    'marker_custom_description'
+  );
+
+  add_settings_field(
+    'marker_custom_description_field',
+    __( 'Marker description:'),
+    'cpa_map_render_marker_description_input',
+    'map_options_page',
+    'map_main_settings'
+  );
+
 }
 add_action ('admin_init', 'cpa_customize_map_settings');
 
@@ -207,9 +280,22 @@ add_action ('admin_init', 'cpa_customize_map_settings');
  * @return void
  */
 function cpa_map_render_main_settings_section() {
-  echo "<p>Lets begin customizing your map.</p>";
+    echo '<p>Need help figuring out your longitude and latitude? Visit <a href="http://www.geo-tag.de/generator/en.html" target="_blank">Geo Tag Generator.</a></p>';
+    echo '<hr />';
 }
 
+function cpa_map_render_zoom_input() {
+  $mapZoom = get_option( 'marker_custom_zoom', 3 );
+?>
+  <select name="marker_custom_zoom">
+    <option value="1" <?php selected( 1, $mapZoom ); ?>>1</option>
+    <option value="3" <?php selected( 3, $mapZoom ); ?>>2</option>
+    <option value="8" <?php selected( 8, $mapZoom ); ?>>3</option>
+    <option value="13" <?php selected( 13, $mapZoom ); ?>>4</option>
+    <option value="18" <?php selected( 18, $mapZoom ); ?>>5</option>
+  </select>
+<?php
+}
 /**
  * Render the input for custom marker location.
  *
@@ -219,7 +305,7 @@ function cpa_map_render_main_settings_section() {
  */
 function cpa_map_render_custom_marker_input() {
   $markerlonLocation = get_option( 'marker_custom_lon' );
-  echo '<label>Lon</label> <input name="marker_custom_lon"  type="text" value=" '. $markerlonLocation .' " />';
+  echo '<label>Lat.</label> <input name="marker_custom_lon"  type="text" value=" '. $markerlonLocation .' " />';
 }
 
 /**
@@ -231,7 +317,7 @@ function cpa_map_render_custom_marker_input() {
  */
 function cpa_map_render_custom_marker_lat_input() {
   $markerlatLocation = get_option( 'marker_custom_lat' );
-  echo '<label>Lon</label> <input name="marker_custom_lat"  type="text" value=" '. $markerlatLocation .' " />';
+  echo '<label>Lon.</label> <input name="marker_custom_lat"  type="text" value=" '. $markerlatLocation .' " />';
 }
 
 /**
@@ -242,9 +328,33 @@ function cpa_map_render_custom_marker_lat_input() {
  * @return void
  */
 function cpa_map_render_custom_map_input() {
-  $mapID = get_option( 'marker_custom_hex');
-  echo '<input name="marker_custom_hex"  type="text" value=" '. $mapID .' " />';
+
+  $mapID = get_option( 'marker_custom_hex' );
+  ?>
+  <input name="marker_custom_hex"  type="text" value="d50f25<?php checked( $mapID, false);?>"  />
+  <?php
 }
 
+/**
+ * Render the input for custom marker color.
+ *
+ * @since  1.0.
+ *
+ * @return void
+ */
+function cpa_map_render_marker_title_input() {
+  $markerTitle = get_option( 'marker_custom_title');
+  echo '<input name="marker_custom_title"  type="text" value=" '. $markerTitle .' " />';
+}
 
-
+/**
+ * Render the input for custom marker color.
+ *
+ * @since  1.0.
+ *
+ * @return void
+ */
+function cpa_map_render_marker_description_input() {
+  $markerDescription = get_option( 'marker_custom_description');
+  echo '<textarea name="marker_custom_description"   value=" '. $markerDescription .' " ></textarea>';
+}
